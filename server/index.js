6f6b52c9d3abf58798d9044ad8e31c40a652e97d
Monchai <monchai.j@seven.co.th>
2018-05-23 15:22:04 +0700

    add static public folder

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-
-app.get('/',(req,res)=>{
-    res.send("hello");
-});
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.listen(port,()=>{
     console.log(`server run at ${port}`);
